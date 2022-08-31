027370d3c57a650f3f5d9aec85a77f2f92313122
GaMa404 <luzettileonardo@gmail.com>
2022-08-31 12:22:01 -0300

    Adicionar arquivos de projeto.

---
+++ FormBaixa/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace FormBaixa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvVenda = new System.Windows.Forms.DataGridView();
+            this.btnImportar = new System.Windows.Forms.Button();
+            this.ofdArquivo = new System.Windows.Forms.OpenFileDialog();
+            this.btnMarcar = new System.Windows.Forms.Button();
+            this.btnDesmarcar = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAumento = new System.Windows.Forms.TextBox();
+            this.btnAumentar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVenda)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvVenda
+            // 
+            this.dgvVenda.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVenda.Location = new System.Drawing.Point(12, 81);
+            this.dgvVenda.Name = "dgvVenda";
+            this.dgvVenda.Size = new System.Drawing.Size(857, 410);
+            this.dgvVenda.TabIndex = 0;
+            this.dgvVenda.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvVenda_CellClick);
+            // 
+            // btnImportar
+            // 
+            this.btnImportar.Location = new System.Drawing.Point(12, 22);
+            this.btnImportar.Name = "btnImportar";
+            this.btnImportar.Size = new System.Drawing.Size(118, 43);
+            this.btnImportar.TabIndex = 1;
+            this.btnImportar.Text = "Importar Arquivo";
+            this.btnImportar.UseVisualStyleBackColor = true;
+            this.btnImportar.Click += new System.EventHandler(this.btnImportar_Click);
+            // 
+            // ofdArquivo
+            // 
+            this.ofdArquivo.FileName = "ofdArquivo";
+            // 
+            // btnMarcar
+            // 
+            this.btnMarcar.Location = new System.Drawing.Point(611, 22);
+            this.btnMarcar.Name = "btnMarcar";
+            this.btnMarcar.Size = new System.Drawing.Size(118, 43);
+            this.btnMarcar.TabIndex = 2;
+            this.btnMarcar.Text = "Marcar Todos";
+            this.btnMarcar.UseVisualStyleBackColor = true;
+            this.btnMarcar.Click += new System.EventHandler(this.btnMarcar_Click);
+            // 
+            // btnDesmarcar
+            // 
+            this.btnDesmarcar.Location = new System.Drawing.Point(751, 22);
+            this.btnDesmarcar.Name = "btnDesmarcar";
+            this.btnDesmarcar.Size = new System.Drawing.Size(118, 43);
+            this.btnDesmarcar.TabIndex = 3;
+            this.btnDesmarcar.Text = "Desmarcar Todos";
+            this.btnDesmarcar.UseVisualStyleBackColor = true;
+            this.btnDesmarcar.Click += new System.EventHandler(this.btnDesmarcar_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(739, 518);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(130, 55);
+            this.btnRemover.TabIndex = 4;
+            this.btnRemover.Text = "Remover Selecionados";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(252, 531);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(319, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Aumento (%) ao preço da venda: ";
+            // 
+            // txtAumento
+            // 
+            this.txtAumento.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAumento.Location = new System.Drawing.Point(543, 530);
+            this.txtAumento.Name = "txtAumento";
+            this.txtAumento.Size = new System.Drawing.Size(100, 29);
+            this.txtAumento.TabIndex = 5;
+            // 
+            // btnAumentar
+            // 
+            this.btnAumentar.Location = new System.Drawing.Point(649, 523);
+            this.btnAumentar.Name = "btnAumentar";
+            this.btnAumentar.Size = new System.Drawing.Size(75, 49);
+            this.btnAumentar.TabIndex = 6;
+            this.btnAumentar.Text = "Aumentar";
+            this.btnAumentar.UseVisualStyleBackColor = true;
+            this.btnAumentar.Click += new System.EventHandler(this.btnAumentar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 602);
+            this.Controls.Add(this.btnAumentar);
+            this.Controls.Add(this.txtAumento);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnDesmarcar);
+            this.Controls.Add(this.btnMarcar);
+            this.Controls.Add(this.btnImportar);
+            this.Controls.Add(this.dgvVenda);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVenda)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvVenda;
+        private System.Windows.Forms.Button btnImportar;
+        private System.Windows.Forms.OpenFileDialog ofdArquivo;
+        private System.Windows.Forms.Button btnMarcar;
+        private System.Windows.Forms.Button btnDesmarcar;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAumento;
+        private System.Windows.Forms.Button btnAumentar;
+    }
+}
+
---
+++ FormBaixa/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormBaixa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvVenda.Columns.Insert(0, new DataGridViewCheckBoxColumn());
+            dgvVenda.Columns.Insert(1, new DataGridViewTextBoxColumn());
+            dgvVenda.Columns.Insert(2, new DataGridViewTextBoxColumn());
+            dgvVenda.Columns.Insert(3, new DataGridViewTextBoxColumn());
+            dgvVenda.Columns.Insert(4, new DataGridViewTextBoxColumn());
+            dgvVenda.Columns.Insert(5, new DataGridViewTextBoxColumn());
+            dgvVenda.Columns.Insert(6, new DataGridViewTextBoxColumn());
+
+            dgvVenda.Columns[0].Name = "Selecionar";
+            dgvVenda.Columns[1].Name = "ID";
+            dgvVenda.Columns[2].Name = "EAN";
+            dgvVenda.Columns[3].Name = "Produto";
+            dgvVenda.Columns[4].Name = "Valor Compra";
+            dgvVenda.Columns[5].Name = "Valor Venda";
+            dgvVenda.Columns[6].Name = "Estoque";
+
+            dgvVenda.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dgvVenda.ReadOnly = true;
+            dgvVenda.AllowUserToAddRows = false;
+            dgvVenda.AllowUserToDeleteRows = false;
+            dgvVenda.AllowUserToOrderColumns = true;
+
+            dgvVenda.Columns[4].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dgvVenda.Columns[4].DefaultCellStyle.Format = " R$ ###,###,##0.00";
+
+            dgvVenda.Columns[5].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dgvVenda.Columns[5].DefaultCellStyle.Format = "R$ ###,###,##0.00";
+        }
+
+        private void dgvVenda_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(dgvVenda.RowCount > 0 && e.ColumnIndex == 0)
+            {
+                if (Convert.ToBoolean(dgvVenda.CurrentRow.Cells[0].Value) == false)
+                {
+                    dgvVenda.CurrentRow.Cells[0].Value = true;
+                }
+                else 
+                {
+                    dgvVenda.CurrentRow.Cells[0].Value = false;
+                }
+            }
+        }
+
+        private void btnImportar_Click(object sender, EventArgs e)
+        {
+            dgvVenda.RowCount = 0;
+
+            ofdArquivo.FileName = "";
+            ofdArquivo.ShowDialog();
+
+            StreamReader arquivo = File.OpenText(ofdArquivo.FileName);
+
+            String linha;
+            
+            while ((linha = arquivo.ReadLine()) != null)
+            {
+                string[] dados = linha.Split(';');
+                string id = dados[0];
+                string ean = dados[1];
+                string produto = dados[2];
+                double valor_compra = double.Parse(dados[3]);
+                double valor_venda = double.Parse(dados[4]);
+                string estoque = dados[5];
+
+                dgvVenda.Rows.Add(false, id, ean, produto, valor_compra, valor_venda, estoque);
+            }
+        }
+
+        private void btnMarcar_Click(object sender, EventArgs e)
+        {
+            foreach(DataGridViewRow linha in dgvVenda.Rows)
+            {
+                linha.Cells[0].Value = true;
+            }
+        }
+
+        private void btnDesmarcar_Click(object sender, EventArgs e)
+        {
+            foreach(DataGridViewRow linha in dgvVenda.Rows)
+            {
+                linha.Cells[0].Value = false;
+            }
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            for (int i = dgvVenda.RowCount - 1; i >= 0; i--)
+            {
+                if (Convert.ToBoolean(dgvVenda.Rows[i].Cells[0].Value) == true)
+                {
+                    dgvVenda.Rows.Remove(dgvVenda.Rows[i]);
+                }
+            }
+        }
+
+        private void btnAumentar_Click(object sender, EventArgs e)
+        {
+            double aumento = double.Parse(txtAumento.Text)/100;
+
+            foreach(DataGridViewRow row in dgvVenda.Rows)
+            {
+                if (Convert.ToBoolean(row.Cells[0].Value) == true)
+                {
+                    row.Cells[5].Value = (Convert.ToDouble(row.Cells[5].Value) + Convert.ToDouble(row.Cells[5].Value) * aumento).ToString("C");
+
+                    /*double preço_antigo = Convert.ToDouble(row.Cells[5].Value);
+                    double preço_atual = preço_antigo + preço_antigo * (aumento / 100);
+
+                    row.Cells[5].Value = preço_atual.ToString("C");
+
+                    preço_antigo = 0;
+                    preço_atual = 0;*/
+                    
+                }
+            }
+
+            txtAumento.Clear();
+        }
+    }
+}
